1a823277fd2c9407ac787deb3c01949723385d68
Thomas Schoffelen <thomas@scholica.com>
2018-04-17 16:09:43 -0600

    Added logic shit

--- index.js
+++ index.js
@@ -1,15 +1,61 @@
 var express = require("express");
 var app = express();
+var docker = new (require("dockerode"))();
 var server = require("http").createServer(app);
-var io = require("socket.io")(server, {pingInterval: 15000, pingTimeout: 30000});
+var uuid = require("uuid/v4");
+var io = require("socket.io")(server, {
+  pingInterval: 15000,
+  pingTimeout: 30000
+});
 var path = require("path");
+var fs = require("fs");
 
 app.use(express.static(__dirname + "/public"));
 
-app.get("/", auth.auth, function(req, res, next) {
+app.get("/", function(req, res, next) {
   res.sendFile(__dirname + "/public/index.html");
 });
 
-var port = process.env.PORT || 4400;
+io.on("connection", function(socket) {
+  console.log("a user connected");
+
+  socket.on("disconnect", function() {
+    console.log("user disconnected");
+  });
+
+  socket.on("run", function(data) {
+    // ask client to clean ouput
+    socket.emit("output.clean");
+
+    // first temporarily store file
+    var id = uuid();
+    var dir = "/tmp/" + id;
+    var filename = "input.rho";
+    fs.mkdir(dir, 0o777, function() {
+      fs.writeFile(dir + "/" + filename, data.body, "utf8", function() {
+        // run docker
+        var image = "rchain/rnode:" + (data.version || "latest");
+        docker
+          .run(image, ["--eval", "/tmp/" + filename, "--mount",
+            'type=bind,source="' + dir + '",target=/tmp'], process.stdout, [
+            "--mount",
+            'type=bind,source="' + dir + '",target=/tmp'
+          ])
+          .then(function(container) {
+            console.log(container.output);
+            return container.remove();
+          })
+          .then(function(data) {
+            console.log("container removed");
+          })
+          .catch(function(err) {
+            console.log(err);
+          });
+      });
+    });
+  });
+});
+
+var port = process.env.PORT || 8000;
 server.listen(port);
 console.log("Server started on port", port);
