3244b0d1460f261a4f0e6627ad2ce098710101f9
Ahsan Fazal <ahsanfazal98@gmail.com>
2018-04-17 18:09:06 -0600

    Ready for the real world.

--- public/editor.js
+++ public/editor.js
@@ -5,3 +5,25 @@ var myCodeMirror = CodeMirror.fromTextArea(document.getElementById('mirror'), {
   mode: 'php',
   theme: 'solarized'
 })
+// Run event triggered by user
+function run () {
+  socket.emit('run', {version: 'latest', body: myCodeMirror.getValue()})
+  document.getElementById('runButton').innerHTML = 'Loading...'
+}
+// Socket.io stuff
+var socket = io('http://rchain.cloud/')
+socket.on('output.clean', function () { clearConsole() })
+socket.on('output.append', function (message) { appendToConsole(message) })
+socket.on('output.done', function (message) { loadingDone() })
+// Functions for callbacks from socket.io
+function clearConsole () {
+  document.getElementById('consoleText').innerHTML = ''
+}
+
+function appendToConsole (message) {
+  document.getElementById('consoleText').innerHTML += message
+}
+
+function loadingDone () {
+  document.getElementById('runButton').innerHTML = 'Run'
+}
