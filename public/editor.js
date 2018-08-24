2bcce87a49f572e6764e5611d4b5ea38cb72290f
Andrijan Ostrun <andrijan.ostrun@gmail.com>
2018-08-24 12:33:53 +0200

    Code polishing

--- public/editor.js
+++ public/editor.js
@@ -413,9 +413,14 @@ $('#tree').on('nodeSelected', function(event, data) {
   */
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function(){
-    if(this.readyState == 4 && this.status == 200){
-      console.log(this.responseText);
-      myCodeMirror.setValue(this.responseText);
+    if(this.readyState == 4){
+      if(this.status == 200){
+        //console.log(this.responseText);
+        myCodeMirror.setValue(this.responseText);
+      }else{
+        console.log("Error: ", xhttp.statusText);
+      }
+      
     }
   };
   xhttp.open("GET", path, true);
